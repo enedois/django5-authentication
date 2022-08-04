4e0cefa8ea7525e1d0134590536d52ce7e740dc1
Renan Zanesco Comoli <rzcrenan@gmail.com>
2022-08-04 18:59:45 +1000

    disabled debugging

--- project5/myproject/myproject/settings.py
+++ project5/myproject/myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&gdql@88g2ks6#f4-wyh@jf+b2fo*@44ca7i)^!&8qv199_wc7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
